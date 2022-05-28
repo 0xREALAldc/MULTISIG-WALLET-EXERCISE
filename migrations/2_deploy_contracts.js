89451b0a0429671167927b436e6a3def77a34c30
REALaldc <realaldc@REALaldcs-MacBook-Pro.local>
2022-05-27 22:03:38 -0300

    Multi signature wallet exercise following step by step insteuctions

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+var MultiSig = artifacts.require("MultiSignatureWallet")
+var SimpleStorage = artifacts.require("SimpleStorage")
+
+module.exports = function(deployer, network, accounts) {
+
+    const owners = [accounts[0], accounts[1], accounts[2], accounts[3], accounts[4]]
+
+    deployer.deploy(SimpleStorage)
+    deployer.deploy(MultiSig, owners, 3)
+};
